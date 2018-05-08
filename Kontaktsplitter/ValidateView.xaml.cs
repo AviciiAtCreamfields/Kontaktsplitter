51c04cfe9f03a76968e3e89b27c109eb0f679dcc
Nils Lohmiller <i15015@hb.dhbw-stuttgart.de>
2018-05-08 14:13:59 +0200

    Erster Commit

--- Kontaktsplitter/ValidateView.xaml.cs
+++ Kontaktsplitter/ValidateView.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Kontaktsplitter
 {
     /// <summary>
-    /// Interaktionslogik für MainWindow.xaml
+    /// Interaktionslogik für ValidateView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ValidateView : Window
     {
-        public MainWindow()
+        public ValidateView()
         {
             InitializeComponent();
         }
